ddb197e9372ac47b4eaa7c23d69f34ad32b80142
Niha472 <72551894+Niha472@users.noreply.github.com>
2021-08-03 16:12:35 +0530

    Add files via upload

---
+++ kwitter_page.js
@@ -0,0 +1,31 @@
+//YOUR FIREBASE LINKS
+var firebaseConfig = {
+      apiKey: "AIzaSyBJBcJzYt4W6pco9Lc5Oc6gKhMevMJEou4",
+      authDomain: "kwitter-86413.firebaseapp.com",
+      databaseURL: "https://kwitter-86413-default-rtdb.firebaseio.com",
+      projectId: "kwitter-86413",
+      storageBucket: "kwitter-86413.appspot.com",
+      messagingSenderId: "870904047141",
+      appId: "1:870904047141:web:479d01b15be8785a5481cc"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  user_name=localStorage.getItem("user_name")
+  room_name=localStorage.getItem("room_name")
+function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+         firebase_message_id = childKey;
+         message_data = childData;
+//Start code
+
+//End code
+      } });  }); }
+getData();
+function send(){
+      msg= document.getElementById("msg").value;
+firebase.database().ref(room_name).push({
+    name:user_name,
+    message:msg,
+    like:0
+});
+document.getElementById(msg).value
+}
